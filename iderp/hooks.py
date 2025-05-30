46e25cd148ae0d4f54f8760424caff5ddecf2ec5
haringk <haringk@gmail.com>
2025-05-30 15:20:44 +0000

    Fix: Corretto hooks.py e aggiunto __version__ in __init__.py
    - Risolto problema caricamento JavaScript sui doctype
    - Aggiunta versione in iderp/__init__.py
    - Configurato doctype_js per Quotation, Sales Order, etc.
    - Plugin ora funziona correttamente in ERPNext 14

--- iderp/hooks.py
+++ iderp/hooks.py
@@ -5,23 +5,19 @@ app_description = "Custom app for ERPNext"
 app_email = "ai@idstudio.org"
 app_license = "MIT"
 
-app_include_js = "/assets/iderp/js/iderp.js"
-app_include_css = "/assets/iderp/css/iderp.css"
-
 doctype_js = {
-    "Quotation": "iderp/public/js/item_dimension.js",
-    "Sales Order": "iderp/public/js/item_dimension.js",
-    "Sales Invoice": "iderp/public/js/item_dimension.js",
-    "Delivery Note": "iderp/public/js/item_dimension.js",
-    "Work Order": "iderp/public/js/item_dimension.js",
-    "Web Order": "iderp/public/js/item_dimension.js"
+    "Quotation": "public/js/item_dimension.js",
+    "Sales Order": "public/js/item_dimension.js",
+    "Sales Invoice": "public/js/item_dimension.js",
+    "Delivery Note": "public/js/item_dimension.js",
+    "Work Order": "public/js/item_dimension.js"
 }
 
 doc_events = {
     "Quotation": {"before_submit": "iderp.copy_fields.copy_custom_fields"},
     "Sales Order": {"before_submit": "iderp.copy_fields.copy_custom_fields"},
     "Delivery Note": {"before_submit": "iderp.copy_fields.copy_custom_fields"},
-    "Sales Invoice": {"before_submit": "iderp.copy_fields.copy_custom_fields"},
+    "Sales Invoice": {"before_submit": "iderp.copy_fields.copy_custom_fields"}
 }
 
 after_install = "iderp.install.after_install"
--- iderp/public/js/item_dimension.js
+++ iderp/public/js/item_dimension.js
@@ -1,94 +1,17 @@
 console.log("IDERP: JS LOADED");
 
-/*
-		'Quotation Item',
-		'Sales Order Item', 
-		'Sales Invoice Item', 
-		'Delivery Note Item', 
-		'Work Order Item', 
-		'Web Order Item'], 
-*/
-
-
 frappe.ui.form.on('Quotation Item', {
-	
-    base: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-    altezza: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-    prezzo_mq: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-
-    // Esegui anche al refresh (quando carica)
+    base: function(frm, cdt, cdn) { 
+        calculate_mq(frm, cdt, cdn); 
+    },
+    altezza: function(frm, cdt, cdn) { 
+        calculate_mq(frm, cdt, cdn); 
+    },
+    prezzo_mq: function(frm, cdt, cdn) { 
+        calculate_mq(frm, cdt, cdn); 
+    },
     refresh: function(frm) {
-        $.each(frm.doc.items || frm.doc.quotation_items || [], function(i, row) {
-            calculate_mq(frm, row.doctype, row.name);
-        });
-    }
-});
-
-frappe.ui.form.on('Sales Order Item', {
-	
-    base: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-    altezza: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-    prezzo_mq: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-
-    // Esegui anche al refresh (quando carica)
-    refresh: function(frm) {
-        $.each(frm.doc.items || frm.doc.quotation_items || [], function(i, row) {
-            calculate_mq(frm, row.doctype, row.name);
-        });
-    }
-});
-
-frappe.ui.form.on('Sales Invoice Item', {
-	
-    base: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-    altezza: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-    prezzo_mq: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-
-    // Esegui anche al refresh (quando carica)
-    refresh: function(frm) {
-        $.each(frm.doc.items || frm.doc.quotation_items || [], function(i, row) {
-            calculate_mq(frm, row.doctype, row.name);
-        });
-    }
-});
-
-frappe.ui.form.on('Delivery Note Item', {
-	
-    base: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-    altezza: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-    prezzo_mq: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-
-    // Esegui anche al refresh (quando carica)
-    refresh: function(frm) {
-        $.each(frm.doc.items || frm.doc.quotation_items || [], function(i, row) {
-            calculate_mq(frm, row.doctype, row.name);
-        });
-    }
-});
-
-frappe.ui.form.on('Work Order Item', {
-	
-    base: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-    altezza: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-    prezzo_mq: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-
-    // Esegui anche al refresh (quando carica)
-    refresh: function(frm) {
-        $.each(frm.doc.items || frm.doc.quotation_items || [], function(i, row) {
-            calculate_mq(frm, row.doctype, row.name);
-        });
-    }
-});
-
-frappe.ui.form.on('Web Order Item', {
-	
-    base: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-    altezza: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-    prezzo_mq: function(frm, cdt, cdn) { calculate_mq(frm, cdt, cdn); },
-
-    // Esegui anche al refresh (quando carica)
-    refresh: function(frm) {
-        $.each(frm.doc.items || frm.doc.quotation_items || [], function(i, row) {
+        $.each(frm.doc.items || [], function(i, row) {
             calculate_mq(frm, row.doctype, row.name);
         });
     }
@@ -97,12 +20,8 @@ frappe.ui.form.on('Web Order Item', {
 function calculate_mq(frm, cdt, cdn) {
     var row = locals[cdt][cdn];
     
-    // Attiva i log
-	console.log("Valori della riga:", row);
-	console.log("MQ Calcolati:", row.mq_calcolati);
-	console.log("Prezzo riga:", row.rate);
+    console.log("Valori della riga:", row);
     
-    // Esegui il calcolo solo se i campi esistono
     if (row.base && row.altezza) {
         row.mq_calcolati = (row.base * row.altezza) / 10000;
         if (row.prezzo_mq) {
@@ -115,45 +34,4 @@ function calculate_mq(frm, cdt, cdn) {
         }
     }
     frm.refresh_field("items");
-    cur_frm.refresh_fields();
 }
-
-
-
-/* 
-
-[
-    'Quotation Item',
-    'Sales Order Item',
-    'Sales Invoice Item',
-    'Delivery Note Item',
-    'Work Order Item',
-    'Web Order Item'
-].forEach(function(dt) {
-    frappe.ui.form.on(dt, {
-        base: function(frm, cdt, cdn) {
-            calculate_importo(frm, cdt, cdn);
-        },
-        altezza: function(frm, cdt, cdn) {
-            calculate_importo(frm, cdt, cdn);
-        },
-        prezzo_mq: function(frm, cdt, cdn) {
-            calculate_importo(frm, cdt, cdn);
-        }
-    });
-});
-
-function calculate_importo(frm, cdt, cdn) {
-    var row = locals[cdt][cdn];
-    if(row.base && row.altezza && row.prezzo_mq) {
-        row.totale_mq = (row.base * row.altezza / 10000).toFixed(3); // se misuri in cm
-        row.importo_mq = (row.totale_mq * row.prezzo_mq).toFixed(2);
-    } else {
-        row.totale_mq = 0;
-        row.importo_mq = 0;
-    }
-    frm.refresh_field("totale_mq", cdn);
-    frm.refresh_field("importo_mq", cdn);
-}
-
-*/
