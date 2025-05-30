bb5d4dda75ec3c07580a4ba4faaf66399c9f99e1
haringk <haringk@gmail.com>
2025-05-30 10:16:00 +0200

    Update item_dimension.js

--- iderp/public/js/item_dimension.js
+++ iderp/public/js/item_dimension.js
@@ -1,3 +1,57 @@
+frappe.ui.form.on(
+	[
+		'Quotation Item',
+		'Sales Order Item', 
+		'Sales Invoice Item', 
+		'Delivery Note Item', 
+		'Work Order Item', 
+		'Web Order Item'], 
+	{
+	
+    base: function(frm, cdt, cdn) {
+        calculate_mq(frm, cdt, cdn);
+    },
+    altezza: function(frm, cdt, cdn) {
+        calculate_mq(frm, cdt, cdn);
+    },
+    prezzo_mq: function(frm, cdt, cdn) {
+        calculate_mq(frm, cdt, cdn);
+    },
+    // Esegui anche al refresh (quando carica)
+    refresh: function(frm) {
+        $.each(frm.doc.items || frm.doc.quotation_items || [], function(i, row) {
+            calculate_mq(frm, row.doctype, row.name);
+        });
+    }
+});
+
+function calculate_mq(frm, cdt, cdn) {
+    var row = locals[cdt][cdn];
+    
+    // Attiva i log
+	console.log("Valori della riga:", row);
+	console.log("MQ Calcolati:", row.mq_calcolati);
+	console.log("Prezzo riga:", row.rate);
+    
+    // Esegui il calcolo solo se i campi esistono
+    if (row.base && row.altezza) {
+        row.mq_calcolati = (row.base * row.altezza) / 10000;
+        if (row.prezzo_mq) {
+            row.rate = row.mq_calcolati * row.prezzo_mq;
+        }
+    } else {
+        row.mq_calcolati = 0;
+        if (row.prezzo_mq) {
+            row.rate = 0;
+        }
+    }
+    frm.refresh_field("items");
+}
+
+
+
+/* 
+
 [
     'Quotation Item',
     'Sales Order Item',
@@ -31,3 +85,5 @@ function calculate_importo(frm, cdt, cdn) {
     frm.refresh_field("totale_mq", cdn);
     frm.refresh_field("importo_mq", cdn);
 }
+
+*/
