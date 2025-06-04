fa61e567fb3add27ccd254fad81e1eb8a0560d24
haringk <74597541+haringk@users.noreply.github.com>
2025-06-04 22:36:04 +0200

    Update __init__.py

--- iderp/__init__.py
+++ iderp/__init__.py
@@ -1,1 +1,16 @@
-__version__ = "0.0.1"
+# __init__.py
+"""
+IDERP - Sistema Stampa Digitale per ERPNext 15
+Versione 2.0 - Compatible with ERPNext 15+
+"""
+
+__version__ = '2.0.0'
+__app_name__ = 'iderp'
+__title__ = 'IDERP - Sistema Stampa Digitale'
+__description__ = 'Plugin ERPNext per stampa digitale con calcoli universali Metro Quadrato/Lineare/Pezzo'
+__author__ = 'idstudio AI'
+__email__ = 'ai@idstudio.org'
+__license__ = 'MIT'
+
+# ERPNext 15 compatibility
+required_apps = ['frappe', 'erpnext']
